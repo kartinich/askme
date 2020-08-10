6b28d03be3045799ec430463b3f8d556530e2735
kartinich <kartinich@MacBook-Pro.local>
2020-08-10 18:58:02 +0300

    Log_in session fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   before_action :load_user, except: [:index, :create, :new]
   before_action :authorize_user, except: [:index, :new, :create, :show]
 
@@ -14,12 +14,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
   	@user = User.new(user_params)
 
   	if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
     else
-      render :edit
+      render :new
   	end
   end
 
@@ -53,7 +55,7 @@ class UsersController < ApplicationController
   def load_user
     @user ||= User.find params[:id]
   end
-  	
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation,
                                  :name, :username, :avatar_url)
