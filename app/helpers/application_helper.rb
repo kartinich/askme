8f6635c3ad9b8434bd36fc437ed5b456772a0c09
kartinich <kartinich@MacBook-Pro.local>
2020-08-10 18:29:30 +0300

    Inclination for questions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,19 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+
+  def inclination(number, form1, form2, form3)
+    option_1 = number % 10
+    option_2 = number % 100
+
+      if (11..14).include?(option_2)
+        return form3
+      elsif option_1 == 1
+        return form1
+      elsif (2..4).include?(option_1)
+        return form2
+      else
+        return form3
+      end
+  end
 end
