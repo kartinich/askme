ce38b8a31d069cecce9e7453d446d1db2e584007
kartinich <dkostrikin@inbox.ru>
2023-08-17 14:39:40 +0300

    100

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   validates :username, presence: true,
                        uniqueness: true,
                        format: { with: VALID_USERNAME_REGEX },
-                       length: { maximum: 50 }
+                       length: { maximum: 100 }
 
   validates :email, presence: true,
                     uniqueness: true,
